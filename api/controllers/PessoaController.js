f96813e6b59cd806d376ad2864a71e4f123ddb80
Joacil Amarante <joacil.amarante@gmail.com>
2022-08-03 06:41:22 -0400

    .

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -22,6 +22,46 @@ class PessoaController{
       return res.status(500).json(error.message)
     }
   }
+  static async criarPessoa(req,res){
+    const novaPessoa = req.body
+    try{
+      const novaPessoaCriada = await database.Pessoas.create(novaPessoa)
+      return res.status(200).json(novaPessoaCriada)
+    }catch(error){
+      return res.status(500).json(error.message)
+    }
+  }
+  static async atualizarPessoa(req,res){
+    const {id} = req.params
+    const novasInfos = req.body
+    try{
+      //atualiza
+      await database.Pessoas.update(novasInfos, {
+        where:{
+          id: Number(id)
+        }
+      })
+      //busca pessoa atualizada
+      const pessoaAtualizada = await database.Pessoas.findOne({
+        where: {
+          id: Number(id)
+        }
+      })
+      return res.status(200).json(pessoaAtualizada)
+    }catch(error){
+      return res.status(500).json(error.message)
+    }
+  }
+
+  static async deletarPessoa(req,res){
+    const {id} = req.params
+    try{
+      await database.Pessoas.destroy({where: {id:Number(id)}})
+      return res.status(200).json({mensagem:`id ${id} deletado`})
+    }catch(error){
+      return res.status(500).json(error.message)
+    }
+  }
 }
 
 module.exports = PessoaController
--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -5,5 +5,8 @@ const router = Router()
 
 router.get('/pessoas', PessoaController.listarPessoas)
 router.get('/pessoas/:id', PessoaController.listarUmaPessoa)
+router.post('/pessoas', PessoaController.criarPessoa)
+router.put('/pessoas/:id', PessoaController.atualizarPessoa)
+router.delete('/pessoas/:id', PessoaController.deletarPessoa)
 
 module.exports = router
